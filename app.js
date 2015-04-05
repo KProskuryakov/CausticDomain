61e0093e575ceca63e9b0b8b24ed831e0134bdd3
Kostyantyn Proskuryakov <kostyapro@gmail.com>
2015-04-05 19:03:52 -0400

    Added index.html for the client and implemented very basic socket.io functionality in app.js and index.html

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 /**
  * Created by Kostya on 4/5/2015.
  */
+app = require('express')();
+server = require('http').Server(app);
+io = require('socket.io')(server);
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
