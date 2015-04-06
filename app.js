23129ce2e1118432c54b7447f1327f9d0c7fa9e7
Kostyantyn Proskuryakov <kostyapro@gmail.com>
2015-04-06 14:33:43 -0400

    Working prototype of WASD controlled players

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ app.get('/', function (req, res) {
 
 var Player = function(x, y, num, socket) {
     this.x = x; this.y = y;
+    this.vel = 0; this.moveDir = 0;
     this.num = num;
     this.socket = socket;
+};
+
+Player.prototype.getMovePacket = function() {
+    return {x: this.x, y: this.y, vel: this.vel, moveDir: this.moveDir, num: this.num};
 };
 
 var player1 = null;
@@ -26,13 +31,10 @@ var player2 = null;
 
 var Game = {};
 
-Game.updatesPerSecond = 20;
+Game.updatesPerSecond = 60;
 
 Game.update = function () {
-    if (player1 != null && player2 != null) {
-        player1.socket.emit('update', {x: player2.x, y: player2.y, num: 1});
-        player2.socket.emit('update', {x: player1.x, y: player1.y, num: 2});
-    }
+
 };
 
 setInterval(Game.update, 1000 / Game.updatesPerSecond);
@@ -42,13 +44,12 @@ setInterval(Game.update, 1000 / Game.updatesPerSecond);
 io.on('connection', function (socket) {
     console.log("New connection on port 8080.");
     if (player1 == null) {
-        player1 = new Player(0, 0, 1);
-        player1.socket = socket;
+        player1 = new Player(0, 0, 1, socket);
         socket.emit("start", {num: 1});
     } else if (player2 == null) {
-        player2 = new Player(0, 0, 2);
-        player2.socket = socket;
+        player2 = new Player(0, 0, 2, socket);
         socket.emit("start", {num: 2});
+        socket.emit('moveChange', player1.getMovePacket());
     }
 
     socket.on('update', function(data) {
@@ -61,4 +62,24 @@ io.on('connection', function (socket) {
             player2.y = data.y;
         }
     });
+
+    socket.on('moveChange', function(data) {
+        if (data.num == 1) {
+            player1.x = data.x;
+            player1.y = data.y;
+            player1.vel = data.vel;
+            player1.moveDir = data.moveDir;
+            if (player2 != null) {
+                player2.socket.emit('moveChange', player1.getMovePacket());
+            }
+        } else if (data.num == 2) {
+            player2.x = data.x;
+            player2.y = data.y;
+            player2.vel = data.vel;
+            player2.moveDir = data.moveDir;
+            if (player1 != null) {
+                player1.socket.emit('moveChange', player2.getMovePacket());
+            }
+        }
+    })
 });
